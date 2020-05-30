a9ae8c060f5050da8f5b5a830ab5be54bfb600fa
Mattias Andrée <maandree@kth.se>
2020-05-30 22:46:34 +0200

    Add todo about different (bad) behaviour under valgrind
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- sctrace.c
+++ sctrace.c
@@ -140,6 +140,7 @@ main(int argc, char **argv)
 		}
 		/* exec will block until parent attaches */
 		execvp(*argv, &argv[with_argv0]);
+		/* TODO on failure, why does it get into a SIGSEGV loop under valgrind? */
 		eprintf("execvp %s:", *argv);
 	default:
 		orig_pid = pid;
